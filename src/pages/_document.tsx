ba53d78591b5193bee59b527b167f5266c2f98c1
Rellyson Douglas <rellysondouglas2015@gmail.com>
2022-01-19 07:40:28 -0300

    ⚙️ configured typescript, _document and more

---
+++ src/pages/_document.tsx
@@ -0,0 +1,27 @@
+import { ColorModeScript } from '@chakra-ui/react'
+import Document, { Html, Head, NextScript, Main } from 'next/document'
+import { theme } from '../styles/theme'
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&family=Rubik:wght@300;400;500;700&display=swap" rel="stylesheet" />
+          <link rel="shortcut icon" href="/favicon.ico" type="image/icon" />
+          {/* <link rel="manifest" href="/manifest.json" /> */}
+          <meta name="description" content="Controle de alertas de chuvas e outros alertas da Prefeitura de Mossoró" />
+          <title>Mossoró Alerta</title>
+        </Head>
+
+        <body>
+          <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
