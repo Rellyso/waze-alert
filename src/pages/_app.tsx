4964deb0f67dcba154ddc1d025fd0a3ac3bed181
Rellyson Douglas <rellysondouglas2015@gmail.com>
2022-01-20 15:08:17 -0300

    Updated _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,27 @@
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, Collapse, Flex } from '@chakra-ui/react'
 import { AppProps } from 'next/app'
+import { Header } from '../components/Header'
+import { NavigationDrawer } from '../components/NavigationDrawer'
 import { theme } from '../styles/theme'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme} resetCSS>
-      <Component {...pageProps} />
+      <Header />
+
+      <Flex
+        w="100%"
+        h="calc(100vh - 4.5rem)"
+      >
+        <NavigationDrawer />
+
+        <Flex
+          w="100%"
+          className="content"
+        >
+          <Component {...pageProps} />
+        </Flex>
+      </Flex>
     </ChakraProvider>
   )
 }
