3187f7d56b2fff95dd168c6668e99665fffac150
Rellyson Douglas <rellysondouglas2015@gmail.com>
2022-01-20 15:08:29 -0300

    updated pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,35 +1,18 @@
 import { Button, Container, Flex, Heading, Icon, List, Text, UnorderedList } from "@chakra-ui/react";
+import Head from "next/head";
 import { Header } from "../components/Header";
-import { NavDrawerItem } from "../components/NavigationDrawer/NavDrawerItem";
-import { BsHouseDoorFill } from 'react-icons/bs'
 
 
 export default function Home() {
   return (
-    <>
-      <Header />
+    <Flex
+      w="100%"
+    >
+      <Head>
+        <title>Mossoró Alerta</title>
+      </Head>
+      <Text>Waze</Text>
+    </Flex>
 
-      <Flex
-        w="100%"
-        h="calc(100vh - 4.5rem)"
-      >
-        <Flex
-          as="nav"
-          w="264px"
-          borderRight="1px solid #e8eaed"
-          pt="6"
-          direction="column"
-        >
-          <NavDrawerItem href="#" isActive icon={BsHouseDoorFill}>Início</NavDrawerItem>
-          <NavDrawerItem href="#" icon={BsHouseDoorFill}>Novo alerta</NavDrawerItem>
-        </Flex>
-
-        <Flex
-          w="100%"
-        >
-          <Text>Waze</Text>
-        </Flex>
-      </Flex>
-    </>
   )
 }
---
+++ src/pages/novo-alerta.tsx
@@ -0,0 +1,13 @@
+import { Flex, Text } from "@chakra-ui/react";
+import Head from "next/head";
+
+export default function NewAlert() {
+  return (
+    <Flex>
+      <Head>
+        <title>Novo Alerta | Mossoró Alerta</title>
+      </Head>
+      <Text>Novo Alerta</Text>
+    </Flex>
+  )
+}
